91bea9673a359933a5ed3bf7e6ba2ff984f12b5a
zcAllBru <allan.brunner@zeacold.com>
2024-08-28 21:25:12 +0200

    Displayed the parent nodes and retrieve the original of the selected one

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,83 @@
-﻿using System.Text;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace JSONReader
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<TreeNode> ParentNodes { get; set; } = [];
+        public ObservableCollection<TreeNode> Nodes { get; set; } = [];
+
         public MainWindow()
         {
             InitializeComponent();
+
+            JObject json = InitJSON();
+            InitNodes(json);
+            
+            ParentNodes.Clear();
+            foreach (TreeNode node in Nodes)
+            {
+                ParentNodes.Add(node.Clone());
+            }
+
+            FilterOnlyParent(ParentNodes);
+
+            DataContext = this;
+        }
+
+        private static JObject InitJSON()
+        {
+            string path = "C:/temp/example.json";
+            string jsonString = File.ReadAllText(path);
+            return JObject.Parse(jsonString);
+        }
+
+        private void InitNodes(JObject json)
+        {
+            var rootNode = TreeNode.FromJToken("Root", json);
+
+            Nodes = [rootNode];
+        }
+
+        private static void FilterOnlyParent(ObservableCollection<TreeNode> treeNodes)
+        {
+            var nodesToRemove = new List<TreeNode>();
+
+            foreach (var node in treeNodes)
+            {
+                if (node.Children.Count == 0)
+                    nodesToRemove.Add(node);
+                else
+                    FilterOnlyParent(node.Children);
+            }
+
+            foreach (var node in nodesToRemove) 
+                treeNodes.Remove(node);
+        }
+
+        private void trvParentNode_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            List<(string name, string value)> propertiesToDisplay = [];
+
+            TreeNode foundedNode = GetNode((TreeNode)e.NewValue, Nodes);
+        }
+
+        private TreeNode? GetNode(TreeNode nodeToSearch, ObservableCollection<TreeNode> treeNodes)
+        {
+            foreach (var node in treeNodes)
+            {
+                if (node.Id == nodeToSearch.Id)
+                    return node;
+
+                TreeNode foundNode = GetNode((TreeNode)nodeToSearch, node.Children);
+                if (foundNode != null)
+                    return foundNode;
+            }
+
+            return null;
         }
     }
 }
---
+++ TreeNode.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace JSONReader
+{
+    public class TreeNode(string name)
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = name;
+        public ObservableCollection<TreeNode> Children { get; set; } = [];
+
+        public static TreeNode FromJToken(string name, JToken token)
+        {
+            var node = new TreeNode(name);
+
+            if (token is JObject obj)
+            {
+                foreach (var property in obj.Properties())
+                {
+                    var childNode = FromJToken(property.Name, property.Value);
+                        node.Children.Add(childNode);
+                }
+            }
+            else if (token is JArray array)
+            {
+                for (int i = 0; i < array.Count; i++)
+                {
+                    var childNode = FromJToken($"[{i}]", array[i]);
+                        node.Children.Add(childNode);
+                }
+            }
+
+            return node;
+        }
+
+        public TreeNode Clone()
+        {
+            var newNode = new TreeNode(Name)
+            {
+                Id = this.Id
+            };
+            foreach (var child in Children)
+            {
+                newNode.Children.Add(child.Clone());
+            }
+            return newNode;
+        }
+    }
+
+}
